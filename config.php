f2e1162eda32ecd5162c86cd05e3a62b09f8b21e
petitpouletrose <61066556+petitpouletrose@users.noreply.github.com>
2021-08-22 08:41:44 +0200

    change config

--- config.php
+++ config.php
@@ -4,21 +4,21 @@ unset($CFG);
 global $CFG;
 $CFG = new stdClass();
 
-$CFG->dbtype    = getenv('DATABASE_TYPE');
+$CFG->dbtype    = getenv('pgsql');
 $CFG->dblibrary = 'native';
-$CFG->dbhost    = getenv('DATABASE_HOST');
-$CFG->dbname    = getenv('DATABASE_NAME');
-$CFG->dbuser    = getenv('DATABASE_USER');
-$CFG->dbpass    = getenv('DATABASE_PASSWORD');
+$CFG->dbhost    = getenv('ec2-176-34-105-15.eu-west-1.compute.amazonaws.com');
+$CFG->dbname    = getenv('dd1hqstututqac');
+$CFG->dbuser    = getenv('jsnrjfdzzgvjne');
+$CFG->dbpass    = getenv('b7aeefc19458143ffee5cc63b5521ec4957f210e3903e36d0227e1365d0828c3');
 $CFG->prefix    = 'mdl_';
 $CFG->dboptions = array (
   'dbpersist' => 0,
-  'dbport' => getenv('DATABASE_PORT'),
+  'dbport' => getenv('5432'),
   'dbsocket' => '',
 );
 
-$CFG->wwwroot   = getenv('WWWROOT');
-$CFG->dataroot  = getenv('DATAROOT');
+$CFG->wwwroot   = getenv('http://moodleipc.herokuapp.com');
+$CFG->dataroot  = getenv('/tmp');
 $CFG->admin     = 'admin';
 
 $CFG->directorypermissions = 0777;
